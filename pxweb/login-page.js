aabfb6f07de52d8d0a3b26677e74f602d8d0be7a
jnalabol <141327058+jnalabol@users.noreply.github.com>
2023-09-21 14:56:40 +0530

    added a web application

---
+++ pxweb/login-page.js
@@ -0,0 +1,20 @@
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+    (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+  var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+  var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-ULPLJAP59HWA-2");
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "user") {
+        alert("You have successfully logged in.");
+        location.href = "Page.html";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
